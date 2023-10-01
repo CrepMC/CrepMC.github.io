b3baf3c76e9e82c1fe36574c3816c22ba51c4fe5
CrepMC <106760948+CrepMC@users.noreply.github.com>
2023-10-01 16:42:15 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+  document.getElementById("hideLoadingbar").style.display = "none"
+}, 2700);
+setTimeout(function() {
+  document.getElementById("hideLoadingicon").style.display = "none"
+},3500);
+setTimeout(function() {
+  document.getElementById("hideLoadingbar2").style.display = "none"
+}, 2700);
