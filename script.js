55916f81b77ca37aa2ccb5ae44cebee4625606d2
CrepMC <106760948+CrepMC@users.noreply.github.com>
2023-10-01 16:44:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ setTimeout(function() {
 setTimeout(function() {
   document.getElementById("hideLoadingbar2").style.display = "none"
 }, 2700);
+setTimeout(function() {
+    const element1 = document.getElementById('element1');
+    const element2 = document.getElementById('element2');
+    
+    element1.style.display = 'none';
+    element2.style.display = 'block';
+}, 3500);
