3dc4cb0e386c38445d0776a7ad49bab7a04bd4b2
CrepMC <106760948+CrepMC@users.noreply.github.com>
2023-11-24 17:47:56 +0700

    gg

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-setTimeout(function() {
-  document.getElementById("hideLoadingbar").style.display = "none"
-}, 2700);
-setTimeout(function() {
-  document.getElementById("hideLoadingicon").style.display = "none"
-},3500);
-setTimeout(function() {
-  document.getElementById("hideLoadingbar2").style.display = "none"
-}, 2700);
-setTimeout(function() {
-  document.getElementById("hideLoading").style.display = "none"
-},3500);
-setTimeout(function() {
-  const element2 = document.getElementById('element2');
-    
-  element2.style.display = 'block';
+setTimeout(function () {
+  hideElement("hideLoadingbar");
+  hideElement("hideLoadingicon");
+  hideElement("hideLoadingbar2");
+  hideElement("hideLoading");
+  showElement("element2");
+  showElement("wrapshow");
 }, 3500);
-setTimeout(function() {
-  const wrapshow = document.getElementById('wrapshow')
-  
-  wrapshow.style.display = 'block';
-}, 3500);
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
+
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
 });
-document.addEventListener('copy', function(e) {
-    e.preventDefault();
+
+document.addEventListener('copy', function (e) {
+  e.preventDefault();
 });
+
+function hideElement(elementId) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.style.display = 'none';
+  }
+}
+
+function showElement(elementId) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.style.display = 'block';
+  }
+}
