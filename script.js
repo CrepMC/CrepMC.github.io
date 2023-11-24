fb35819af594efbfc32f14adb8220f3ea6766e74
CrepMC <106760948+CrepMC@users.noreply.github.com>
2023-11-24 17:41:51 +0700

    gg

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ setTimeout(function() {
 setTimeout(function() {
   document.getElementById("hideLoadingbar2").style.display = "none"
 }, 2700);
+setTimeout(function() {
   document.getElementById("hideLoading").style.display = "none"
 },3500);
 setTimeout(function() {
